5c23ffa74ae1c6bac74eb3be47fefa4629d2c971
SiyaTM <siyatmahlangu@gmail.com>
2024-10-03 13:15:19 +0200

    first commit

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://83d295e6dd1a53ed0622b461a996c4d3@o4508032431947776.ingest.de.sentry.io/4508032441057360",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
---
+++ tailwindcss.d.ts
@@ -0,0 +1,6 @@
+// tailwindcss.d.ts
+
+declare module "tailwindcss/lib/util/flattenColorPalette" {
+    const flattenColorPalette: any; // Adjust 'any' to a more specific type if possible
+    export default flattenColorPalette;
+  }  
